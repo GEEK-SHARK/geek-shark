cf2b978b635eed0b4918614bce4a31f0f1bb687d
金炳 <hzjinbing@corp.netease.com>
2018-04-21 10:48:07 +0800

    change: 修改

--- src/components/task.vue
+++ src/components/task.vue
@@ -5,31 +5,47 @@
         <h1>基础任务</h1>
       </div>
       <ul>
-        <li>
+        <li class="task_item">
           <div>
-            <span>每日登陆</span>
+            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
+            <span class="task_title">每日登陆</span>
+            <span class="task_description">登录获取金币</span>
+            <span class="task_value">+2金币</span>
           </div>
         </li>
-        <li>
+        <li  class="task_item">
           <div>
+            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
             <span>关注微信公众号</span>
+            <span>登录获取金币</span>
+            <span>+2金币</span>
           </div>
         </li>
-        <li>
+        <li  class="task_item">
           <div>
+            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
             <span>每日微信运动</span>
+            <span>登录获取金币</span>
+            <span>+2金币</span>
           </div>
         </li>
-        <li>
+        <li class="task_item">
           <div>
+            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
             <span>严选购物</span>
+            <span>登录获取金币</span>
+            <span>+2金币</span>
           </div>
         </li>
-        <li>
+        <li class="task_item">
           <div>
+            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
             <span>游戏分享</span>
+            <span>登录获取金币</span>
+            <span>+2金币</span>
           </div>
         </li>
+        <div class="clear_fix"></div>
       </ul>
     </div>
   </div>
@@ -45,20 +61,52 @@ export default {
 </script>
 
 <style scoped>
-.task_container {
-    background: #fff;
+.task_header {
+    border-bottom: 1px solid #ddd;
+    font-size: 14px;
+    padding: 10px;
 }
 
-.task_header {
-    border-bottom: 1px solid #ccc;
-    border-top: 1px solid #ccc;
-    padding: 5px;
+.task_item::after {
+    clear: both;
 }
-.task li {
+
+.task_item img {
+    width: 80px;
+    height: 80px;
+    display: block;
+}
+.task_item img {
+    width: 40px;
+    height: 40px;
+    margin: 0 auto;
+}
+.task_item {
     float: left;
-    width: 32%;
     height: 100px;
     font-size: 12px;
-    border: 1px solid #ccc;
+    width: 100px;
+    overflow: hidden;
+}
+.task_item .task_title {
+    display: block;
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+}
+.task_item .task_description {
+    display: block;
+    text-align: center;
+}
+.task_item .task_value {
+    display: block;
+    text-align: center;
+    font-size: 12px;
+    background: goldenrod;
+    opacity: 0.7;
+    margin: 0 10px;
+}
+.clear_fix {
+    clear: both;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,80 +1,83 @@
 <template>
-  <div class="container" >
-    <div class="header_container">
-        <span class="power">市值</span>
-        <input type="text" value="380万" class="power_value"/>
-        <span class="coin">金币</span>
-        <input type="text" value="279" class="coin_value"/>
-        <span @click="addCoin()" class="add_coin">+</span>
-    </div>
-    <div class="image">
-        <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/u12.jpg" alt="">
-        <div class="view_button">
-        <a href="/pages/share/main">👀</a>
-        </div>
-    </div>
-    <div style="width: 100%; height: 8px;background: #eee;">
-    </div>
-    <div class="game_item_container" v-if="type == 'game_item'">
-        <div class="game_item_header">
-          <h1>待选商品</h1>
-        </div>
-        <ul class="game_item">
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/84f4ac7d46dc4a8814fa4974798d25a0.png" alt="">
-            <span>沙发1</span>
-            </div>
-        </li>
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/4b4dc24c777115efe22478c5601f1ea9%20%281%29.png" alt="">
-            <span>沙发2</span>
-            </div>
-        </li>
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/84f4ac7d46dc4a8814fa4974798d25a0.png" alt="">
-            <span>沙发3</span>
-            </div>
-        </li>
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/a67a775fcb0712b691a492cc387d1dc4.png" alt="">
-            <span>沙发4</span>
-            </div>
-        </li>
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/cb05cb5eb82f0d9c6f5c295731abb7f7.png" alt="">
-            <span>沙发5</span>
-            </div>
-        </li>
-        <li>
-            <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/e21b58dc1fe7de3228ab9f45bbeec0d5.png" alt="">
-            <span>沙发6</span>
-            </div>
-        </li>
-        </ul>
-    </div>
-    <task v-if="type == 'task_items'"></task>
-    <div class="button_groups">
-        <ul>
-        <li>
-            <a href="">
-                <img class="button_img" src="https://cdn1.iconfinder.com/data/icons/finance-banking-tiny-icons/16/shopping-128.png" alt="">
-            </a>
-        </li>
-        <li>
-            <a href="/pages/leaderboard/main"><img class="button_img" src="https://cdn4.iconfinder.com/data/icons/people-37/512/38-128.png" alt=""></a>
-        </li>
-        <li>
-            <button class="share-btn" open-type="share" size="mini" plain="true" @click="share($event)"><img class="button_img" src="https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Share-128.png" alt=""></button>
-        </li>
-        </ul>
+<div class="container">
+  <div class="header_container">
+    <span class="power">市值</span>
+    <input type="text" value="380万" class="power_value" />
+    <span class="coin">金币</span>
+    <input type="text" value="279" class="coin_value" />
+    <span @click="addCoin()" class="add_coin">+</span>
+  </div>
+  <div class="image">
+    <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/u12.jpg" alt="" @click="changeGameItem()">
+    <div class="view_button">
+      <a href="/pages/share/main">👀</a>
     </div>
   </div>
+  <div style="width: 100%; height: 8px;background: #eee;">
+  </div>
+  <div class="game_item_container" v-if="type == 'game_item'">
+    <div class="game_item_header">
+      <h1>待选商品</h1>
+    </div>
+    <ul class="game_item">
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/84f4ac7d46dc4a8814fa4974798d25a0.png" alt="">
+          <span>沙发1</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/4b4dc24c777115efe22478c5601f1ea9%20%281%29.png" alt="">
+          <span>沙发2</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/84f4ac7d46dc4a8814fa4974798d25a0.png" alt="">
+          <span>沙发3</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/a67a775fcb0712b691a492cc387d1dc4.png" alt="">
+          <span>沙发4</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/cb05cb5eb82f0d9c6f5c295731abb7f7.png" alt="">
+          <span>沙发5</span>
+        </div>
+      </li>
+      <li>
+        <div>
+          <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/e21b58dc1fe7de3228ab9f45bbeec0d5.png" alt="">
+          <span>沙发6</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <task v-if="type == 'task_items'"></task>
+
+  <div style="width: 100%; height: 8px;background: #eee;">
+  </div>
+  <div class="button_groups">
+    <ul>
+      <li>
+        <a href="">
+                <img class="button_img" src="https://cdn1.iconfinder.com/data/icons/finance-banking-tiny-icons/16/shopping-128.png" alt="">
+            </a>
+      </li>
+      <li>
+        <a href="/pages/leaderboard/main"><img class="button_img" src="https://cdn4.iconfinder.com/data/icons/people-37/512/38-128.png" alt=""></a>
+      </li>
+      <li>
+        <button class="share-btn" open-type="share" size="mini" plain="true" @click="share($event)"><img class="button_img" src="https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Share-128.png" alt=""></button>
+      </li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
@@ -90,7 +93,7 @@ export default {
         return {
             motto: 'Hello World',
             userInfo: {},
-            type: 'game_item',
+            type: 'task_items',
         }
     },
 
@@ -102,7 +105,9 @@ export default {
     methods: {
         bindViewTap() {
             const url = '../logs/main'
-            wx.navigateTo({url})
+            wx.navigateTo({
+                url,
+            })
         },
         getUserInfo() {
             // 调用登录接口
@@ -123,6 +128,9 @@ export default {
         addCoin() {
             this.type = 'task_items'
         },
+        changeGameItem() {
+            this.type = 'game_item'
+        },
     },
 
     created() {
@@ -136,9 +144,11 @@ export default {
 .container {
     padding: 0;
 }
+
 .header_container {
     padding: 0 10px;
 }
+
 .power {
     display: inline-block;
     height: 40px;
@@ -146,6 +156,7 @@ export default {
     vertical-align: bottom;
     font-size: 14px;
 }
+
 .power_value {
     display: inline-block;
     height: 40px;
@@ -156,6 +167,7 @@ export default {
     padding: 0 10px 0 0;
     font-size: 14px;
 }
+
 .coin {
     display: inline-block;
     height: 40px;
@@ -163,6 +175,7 @@ export default {
     vertical-align: bottom;
     font-size: 14px;
 }
+
 .coin_value {
     display: inline-block;
     width: 128rpx;
@@ -174,6 +187,7 @@ export default {
     padding: 0 10px 0 0;
     font-size: 14px;
 }
+
 .add_coin {
     display: inline-block;
     height: 40px;
@@ -181,10 +195,12 @@ export default {
     vertical-align: bottom;
     font-size: 14px;
 }
+
 .image {
     position: relative;
     padding: 0 10px;
 }
+
 .image .view_button {
     position: absolute;
     right: 10px;
@@ -226,32 +242,38 @@ export default {
 .game_item li span {
     font-size: 12px;
 }
+
 .userinfo {
     display: flex;
     flex-direction: column;
     align-items: center;
 }
+
 .game_item_container {
     overflow: hidden;
     padding: 0 auto;
 }
 
 .game_item_header {
-    border-bottom: 1px solid #ddd;
+    border-bottom: 1px solid #f4f4f4;
     font-size: 14px;
     padding: 10px;
 }
+
 .game_item {
     background: rgb(242, 242, 242);
 }
+
 .game_item::after {
     clear: both;
 }
+
 .game_item img {
     width: 100px;
     height: 100px;
     display: block;
 }
+
 .userinfo-avatar {
     width: 128rpx;
     height: 128rpx;
@@ -282,6 +304,7 @@ export default {
     color: blue;
     border: 1px solid blue;
 }
+
 .button_groups {
     position: fixed;
     bottom: 10px;
@@ -289,20 +312,25 @@ export default {
     height: 40px;
     font-size: 14px;
 }
+
 .button_groups ul {
     overflow: hidden;
 }
+
 .button_groups li {
     float: right;
     margin-left: 10px;
 }
+
 .button_groups li a {
     padding: 0 10px;
 }
+
 .button_groups li .button_img {
     width: 32px;
     height: 32px;
 }
+
 .button_groups li .share-btn {
     border: none;
     padding: 0 10px;
