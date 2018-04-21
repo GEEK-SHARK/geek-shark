1d7bf70bb66ee73cbd112a6170075a57445a4388
unknown <hzwangying1@cn.net.ntes>
2018-04-21 17:58:21 +0800

    change: 任务样式完成

--- src/components/task.vue
+++ src/components/task.vue
@@ -7,45 +7,74 @@
       <ul>
         <li class="task_item">
           <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
-            <span class="task_title">每日登陆</span>
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
+            </span>
+            <span class="task_title">连续登录</span>
             <span class="task_description">登录获取金币</span>
             <span class="task_value">+2金币</span>
           </div>
         </li>
         <li class="task_item task_item2">
           <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105501%402x.png" alt="">
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105501%402x.png" alt="">
+            </span>
             <span class="task_title">关注微信公众号</span>
-            <span class="task_description">登录获取金币</span>
-            <span class="task_value">+2金币</span>
+            <span class="task_description">关注获取金币</span>
+            <span class="task_value">+5金币</span>
           </div>
         </li>
         <li class="task_item task_item3">
           <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105611%402x.png" alt="">
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105611%402x.png" alt="">
+            </span>
             <span class="task_title">每日微信运动</span>
-            <span class="task_description">登录获取金币</span>
+            <span class="task_description">微信步数获取金币</span>
             <span class="task_value">+2金币</span>
           </div>
         </li>
         <li class="task_item">
           <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105519%402x.png" alt="">
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105519%402x.png" alt="">
+            </span>
             <span class="task_title">严选购物</span>
-            <span class="task_description">登录获取金币</span>
+            <span class="task_description">严选购物获取金币</span>
+            <span class="task_value">+5金币</span>
+          </div>
+        </li>
+        <li class="task_item">
+          <div>
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
+            </span>
+            <span class="task_title">游戏分享</span>
+            <span class="task_description">分享游戏获取金币</span>
             <span class="task_value">+2金币</span>
           </div>
         </li>
         <li class="task_item task_item6">
           <div>
-            <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-102904%402x.png" alt="">
-            <span class="task_title">游戏分享</span>
-            <span class="task_description">登录获取金币</span>
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105519%402x.png" alt="">
+            </span>
+            <span class="task_title">早睡打卡</span>
+            <span class="task_description">20:00~22:00打卡获取</span>
             <span class="task_value">+2金币</span>
           </div>
         </li>
-        <li class="task_item"></li>
+        <li class="task_item">
+          <div>
+            <span class="task_img">
+                <img src="https://520stone-blog.oss-cn-beijing.aliyuncs.com/tmp/WX20180421-105519%402x.png" alt="">
+            </span>
+            <span class="task_title">早起打卡</span>
+            <span class="task_description">06:00~08:00打卡获取</span>
+            <span class="task_value">+2金币</span>
+          </div>
+        </li>
       </ul>
     </div>
   </div>
@@ -67,32 +96,25 @@ export default {
     padding: 10px;
 }
 
-.task_item::after {
-    clear: both;
-}
-
 .task ul {
     display: flex;
     flex-wrap: wrap;
-    flex-basis: 3;
-    justify-content: center;
-    align-items: center;
 }
 
 .task_item {
     flex-grow: 1;
-    flex: 0 0 32%;
+    flex: 0 0 33%;
 }
-
-.task_item img {
-    width: 80px;
-    height: 80px;
+.task_item .task_img {
     display: block;
+    width: 40px;
+    height: 40px;
+    margin: 0 auto;
 }
 .task_item img {
     width: 40px;
     height: 40px;
-    margin: 0 auto;
+    display: block;
 }
 .task_item {
     float: left;
@@ -120,7 +142,7 @@ export default {
     font-size: 12px;
     background: goldenrod;
     opacity: 0.7;
-    margin: 0 10px;
+    margin: 5px 10px 0;
 }
 .task_item2 img {
     width: 40px;
@@ -134,7 +156,6 @@ export default {
     border-right: none;
 }
 .task_item6 {
-    border-right: none;
     border-right: none;
 }
 .clear_fix {
